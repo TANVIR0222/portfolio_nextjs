a5e68a9b457712b8882b69fb2aff89279b0b8efb
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-09-22 22:59:56 +0600

    update

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -30,7 +30,7 @@ const PortfolioPage = () => {
       title: "Vanilla Book App",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
       img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-      link: "https://coffee-shop-next-gamma.vercel.app/",
+      link: "https://github.com/TANVIR0222",
     },
     {
       id: 4,
@@ -38,7 +38,7 @@ const PortfolioPage = () => {
       title: "Spotify Music App",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
       img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-      link: "https://coffee-shop-next-gamma.vercel.app/",
+      link: "https://github.com/TANVIR0222",
     },
   ];
   return (
